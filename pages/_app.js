6daccc98b6369d0f2aa13031e78d989b870a230d
francisldn <fnhh04@gmail.com>
2022-01-03 18:39:45 +0000

    add wallet connect button

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import '../styles/globals.css'
 import './app.css';
 import Link from 'next/link';
-import {ErrorBoundary} from 'react-error-boundary';
+import Wallet from './api/wallet'
 
 function NFTMarketplace({Component, pageProps}) {
   
-  const myErrorHandler = () => {
-    console.log("error")
-  }
-
   return (
-    <ErrorBoundary onError={myErrorHandler}>
     <div>
       <nav className='border-b p-6' style={{backgroundColor:'purple'}}>
         <p className='text-4x1 font-bold text-white'>NFT Marketplace</p>
+          <Wallet />
         <div className='flex mt-4 justify-center'>
           <Link href='/'>
             <a className='mr-4'>
@@ -39,7 +35,6 @@ function NFTMarketplace({Component, pageProps}) {
       </nav>
       <Component {...pageProps} />
     </div>
-    </ErrorBoundary>
   )
 }
 
